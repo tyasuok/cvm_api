452c728bb5979a6a29783dee5d92eb98b95c7a4f
Thomas Yasuoka <thomas.yasuoka@outlook.com>
2023-01-02 02:08:16 -0300

    added columns mappings for `inf_diario_cols_order` and cad_fnd_cols_order

--- column_mapping.py
+++ column_mapping.py
@@ -7,9 +7,27 @@ inf_diario_cols = {"tp_fundo": "tipo",
                    "captc_dia": "captacao",
                    "resg_dia": "resgate"
 }
+inf_diario_cols_order = ["data", "cnpj_fundo", "vl_cota",
+                         "pl", "captacao", "resgate",
+                         "tipo", "nr_cotst", "vl_total",
+]
 
 cad_fnd_cols = {"tp_fundo": "tipo",
                 "denom_social": "nome_fundo",
                 "fundo_cotas": "cotas",
                 "fundo_exclusivo": "exclusivo",
 }
+cad_fnd_cols_order = ["cnpj_fundo", "tipo", "nome",
+                      "dt_reg", "dt_const", "cd_cvm",
+                      "dt_cancel", "sit", "dt_ini_sit",
+                      "dt_ini_ativ", "dt_ini_exerc", "dt_fim_exerc",
+                      "classe", "dt_ini_classe", "rentab_fundo",
+                      "condom", "cotas", "exclusivo",
+                      "trib_lprazo", "publico_alvo", "entid_invest",
+                      "taxa_perfm", "inf_taxa_perfm", "taxa_adm",
+                      "inf_taxa_adm", "diretor", "cnpj_admin",
+                      "admin", "pf_pj_gestor", "cpf_cnpj_gestor",
+                      "gestor", "cnpj_auditor", "auditor",
+                      "cnpj_custodiante", "custodiante", "cnpj_controlador",
+                      "controlador",
+]
